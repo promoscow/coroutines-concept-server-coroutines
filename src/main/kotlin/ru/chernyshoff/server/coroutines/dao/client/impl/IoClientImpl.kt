8c77d94ebe6002d2e03b5b9014b9c99c44e94783
Vyacheslav Chernyshoff <slava_rossii@list.ru>
2026-06-13 23:33:52 +0300

    app configs separate docker / local

--- src/main/kotlin/ru/chernyshoff/server/coroutines/dao/client/impl/IoClientImpl.kt
+++ src/main/kotlin/ru/chernyshoff/server/coroutines/dao/client/impl/IoClientImpl.kt
@@ -1,19 +1,21 @@
 package ru.chernyshoff.server.coroutines.dao.client.impl
 
 import kotlinx.coroutines.reactor.awaitSingle
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import ru.chernyshoff.server.coroutines.dao.client.IoClient
 
 @Component
 class IoClientImpl(
-    private val client: WebClient
+    private val client: WebClient,
+    @Value($$"${app.io.host}") private val ioHost: String
 ) : IoClient {
 
     override suspend fun trace(traceId: String): String =
         client
             .get()
-            .uri("http://io:8021/api/io/trace/{traceId}", traceId)
+            .uri("$ioHost/api/io/trace/{traceId}", traceId)
             .retrieve()
             .bodyToMono(String::class.java)
             .awaitSingle()
